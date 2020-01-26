b519d9e9cc41ca9e5139a2c31b4a390e46e2fb20
Vignesh <monuv009@hotmail.co.uk>
2020-01-26 08:22:52 +0530

    Added Modal & Layouts

--- main.js
+++ main.js
@@ -1,20 +1,44 @@
 // Modules
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, ipcMain} = require('electron')
+const windowStateKeeper = require('electron-window-state')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
+// listening to new-item request
+ipcMain.on('new-item', (e, itemUrl) => {
+
+  console.log(itemUrl)
+  // get new item and send back to renderer
+  setTimeout(() => {
+    e.sender.send('new-item-success', 'New item from main process')
+  }, 5000)
+
+})
+
 // Create a new BrowserWindow when `app` is ready
 function createWindow () {
 
+  // win state keeper
+  let state = windowStateKeeper({
+    defaultWidth: 500, defaultHeight: 650
+  })
+
   mainWindow = new BrowserWindow({
-    width: 1000, height: 800,
-    webPreferences: { nodeIntegration: true }
+    x: state.x, y: state.y,
+    width: state.width, height: state.height,
+    minWidth: 350, maxWidth: 650, minHeight:300,
+    webPreferences: {
+      nodeIntegration: true
+    }
   })
 
   // Load index.html into the new BrowserWindow
   mainWindow.loadFile('renderer/main.html')
+
+  // manage the new window state
+  state.manage(mainWindow)
 
   // Open DevTools - Remove for PRODUCTION!
   mainWindow.webContents.openDevTools()
--- renderer/app.js
+++ renderer/app.js
@@ -1,3 +1,68 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+const { ipcRenderer } = require('electron')
+
+// Dom Nodes
+let showModal = document.getElementById('show-modal'),
+    closeModal = document.getElementById('close-modal'),
+    modal = document.getElementById('modal'),
+    addItem = document.getElementById('add-item'),
+    itemUrl = document.getElementById('url')
+
+// disable and enable modal buttons
+const toggleModalButtons = () => {
+
+    // check state of buttons
+    if(addItem.disabled === true){
+        addItem.disabled = false
+        addItem.style.opacity = 1
+        addItem.innerText = 'Add Item'
+        closeModal.style.display = 'inline'
+    } else {
+        addItem.disabled = true
+        addItem.style.opacity = 0.5
+        addItem.innerText = 'Adding...'
+        closeModal.style.display = 'none'
+    }
+}
+
+// show modal
+showModal.addEventListener('click', e => {
+    modal.style.display = 'flex'
+    itemUrl.focus()
+})
+
+// close modal
+closeModal.addEventListener('click', e => {
+    modal.style.display = 'none'
+})
+
+// handle new item
+addItem.addEventListener('click', e => {
+
+    // check a url exists
+    if (itemUrl.value) {
+        
+        // sending a new item url to main process
+        ipcRenderer.send('new-item', itemUrl.value)
+
+        // disable buttons
+        toggleModalButtons()
+
+    }
+})
+
+// listen for new item from main process
+ipcRenderer.on('new-item-success', (e, newItem) => {
+    console.log(newItem)
+
+    // enable buttons
+    toggleModalButtons()
+
+    // hide modal and clear value
+    modal.style.display = 'none'
+    itemUrl.value = ''
+})
+
+// listening for keyboard submit
+itemUrl.addEventListener('keyup', e => {
+    if(e.key === 'Enter') addItem.click()
+})
